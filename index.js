b72aa2079d0971d80c3e08ae639e09eee1d47592
bvabishek <bv.abishek@gmail.com>
2024-01-29 10:20:23 +0530

    updated code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const forever = require("forever");
 
 //setup the server port
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 //parse request data content type application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
